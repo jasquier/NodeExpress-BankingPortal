f18c63c02f82195f2cdd3674bdd0317db87cab28
John A. Squier <jasquier@gmail.com>
2020-07-22 15:24:07 -0400

    Finish the fifth module
    The module is called Creating a Data Access Library

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
+const { accounts, users, writeJSON } = require("./data");
 
 const app = express();
 
@@ -8,12 +9,6 @@ app.set('views', path.join(__dirname, "./views"));
 app.set('view engine', "ejs");
 app.use(express.static(path.join(__dirname, "./public")));
 app.use(express.urlencoded({ extended: true }));
-
-const accountData = fs.readFileSync(path.join(__dirname, "json", "accounts.json"), { encoding: "UTF8" });
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync(path.join(__dirname, "json", "users.json"), { encoding: "UTF8" });
-const users = JSON.parse(userData);
 
 app.get("/", (req, res) => {
     res.render("index", {
@@ -56,8 +51,7 @@ app.post("/transfer", (req, res) => {
     accounts[from].balance -= amount;
     accounts[to].balance += parseInt(amount);
 
-    const accountsJSON = JSON.stringify(accounts);
-    fs.writeFileSync(path.join(__dirname, "json", "accounts.json"), accountsJSON, "UTF8"); 
+    writeJSON();
 
     res.render("transfer", {
         message: "Transfer Completed"
@@ -74,8 +68,7 @@ app.post("/payment", (req, res) => {
     accounts.credit.balance -= parseInt(req.body.amount);
     accounts.credit.available += parseInt(req.body.amount);
 
-    const accountsJSON = JSON.stringify(accounts);
-    fs.writeFileSync(path.join(__dirname, "json", "accounts.json"), accountsJSON, "UTF8");
+    writeJSON();
 
     res.render("payment", {
         message: "Payment Successful",
